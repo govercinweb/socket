0c62f59db47d8cfaffe9e3cda17a1695748c6b00
govercinweb <govercinweb@gmail.com>
2021-06-04 12:56:35 +0300

    Update index.js
    edge satırı düzeneleme

--- index.js
+++ index.js
@@ -63,7 +63,7 @@ app.get('/',(req,res) => {
                             type: item['SN'] ? item['SN'] : null,
                             category: item['L'] ? item['L'] : null,
                             time: item['S'] ? item['S'] : null,
-                            streamId: item['VI'] ? 'https://edge10.xmediaget.com/hls-live/xmlive/_definst_/' + item['VI'] +'/'+ item['VI'] +'.m3u8?whence=2'  : null
+                            streamId: match['VI'] ? 'https://edge10.xmediaget.com/hls-live/xmlive/' + match['VI'] +'/1/mediaplaylist.m3u8'  : null
                         });
                     }
                 });
@@ -80,7 +80,9 @@ app.get('/',(req,res) => {
                             type: match['SN'] ? match['SN'] : null,
                             category: match['L'] ? match['L'] : null,
                             time: match['S'] ? match['S'] : null,
-                            streamId: match['VI'] ? 'https://edge10.xmediaget.com/hls-live/xmlive/_definst_/' + match['VI'] +'/'+ match['VI'] +'.m3u8?whence=2'  : null
+                            streamId: match['VI'] ? 'https://edge10.xmediaget.com/hls-live/xmlive/' + match['VI'] +'/1/mediaplaylist.m3u8'  : null
+
+                                              
                         });
                     });
                 }
